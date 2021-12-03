0d90c2b4cfb3a85ba929ad18c0d787b875e76428
garyb6 <gary.bennett6@outlook.com>
2021-12-03 15:04:58 +0000

    set up controller module

---
+++ controllers/controller.py
@@ -0,0 +1,7 @@
+from app import app
+from flask import render_template
+from models.shopping_list import *
+
+@app.route('/list')
+def shopping_list():
+    return render_template('index.html', title= "Shopping List", shopping_list=shopping_list)
