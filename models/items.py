01670fc69428fd4138e56b18696d66dd1f67a8ad
garyb6 <gary.bennett6@outlook.com>
2021-12-03 15:00:47 +0000

    set up models modules and completed wireframe

---
+++ models/items.py
@@ -0,0 +1,7 @@
+class Items():
+
+    def __init__(self, name, price, quantity, bought):
+        self.name = name 
+        self.price = price
+        self.quantity = quantity
+        self.bought = bought 
---
+++ models/shopping_list.py
@@ -0,0 +1,8 @@
+from models.items import *
+
+item1 = Items("ps5", 500, 1, False)
+item2 = Items("tv", 700, 1, False)
+item3 = Items("ribbon", 2, 2, False)
+item4 = Items("iphone", 1000, 1, True)
+
+shopping_list=[item1, item2, item3, item4]
